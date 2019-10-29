91b6242610e9b20fd25e7e10c50799268abe8789
MarMMcin <em-erce@wp.pl>
2019-10-30 00:13:47 +0100

    error commiy

---
+++ build/precache-manifest.0969662e554c9e349f540ae46ddbee52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463ed60618d53536fa24a50f020d0028",
+    "url": "/addadvert/index.html"
+  },
+  {
+    "revision": "2921dbeb79bd8184a080",
+    "url": "/addadvert/static/css/main.d1f6443b.chunk.css"
+  },
+  {
+    "revision": "7e59c42d40d68911d725",
+    "url": "/addadvert/static/js/2.f6419025.chunk.js"
+  },
+  {
+    "revision": "2921dbeb79bd8184a080",
+    "url": "/addadvert/static/js/main.5d017736.chunk.js"
+  },
+  {
+    "revision": "14ffda90419f85e24af8",
+    "url": "/addadvert/static/js/runtime-main.1229d58c.js"
+  },
+  {
+    "revision": "621aa230cae1da8cb7abc3fbe71a779c",
+    "url": "/addadvert/static/media/logo2.621aa230.jpg"
+  }
+]);
